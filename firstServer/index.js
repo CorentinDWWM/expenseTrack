ebe6cb7d21d179f666d4c2a2779199f701e77970
CorentinDWWM <newsonic62@gmail.com>
2025-05-20 14:20:15 +0200

    push

--- firstServer/index.js
+++ firstServer/index.js
@@ -26,7 +26,7 @@ const routes = require("./routes");
 
 app.use(routes);
 
-app.get("*", (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__DIRNAME, "client", "dist", "index.html"));
 });
 
